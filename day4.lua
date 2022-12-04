ddf0c479836bdfc620d8347c5c562d4a69f19383
Simon Mihevc <simon.mihevc@pm.me>
2022-12-04 12:12:26 +0100

    day 4 using iterators

--- day4.lua
+++ day4.lua
@@ -2,38 +2,40 @@ util = require('util')
 
 day4 = { }
 
-function day4.countIncludedIntervals(filename)
-    -- 2-4,6-8
-    regex = "(%d+)-(%d+),(%d+)-(%d+)"
-    local countIncludedIntervals = 0
-    for line in io.lines(filename) do
+-- rows is an iterator over input data that returns 4 numbers that represent two ranges A-B,C-D
+function day4.rows(filename)
+    local regex = "(%d+)-(%d+),(%d+)-(%d+)"
+    local lineIter = io.lines(filename)
+    return function ()
+        local line = lineIter()
+        if line == nil then
+            return nil
+        end
+
         for lFromRaw, lToRaw, rFromRaw, rToRaw in string.gmatch(line, regex) do
-            lFrom, lTo, rFrom, rTo = math.tointeger(lFromRaw), math.tointeger(lToRaw), math.tointeger(rFromRaw), math.tointeger(rToRaw)
-            local leftContainsRight = lFrom <= rFrom and lTo >= rTo
-            local rightContainsLeft = rFrom <= lFrom and rTo >= lTo
-            if leftContainsRight or rightContainsLeft then
-                countIncludedIntervals = countIncludedIntervals + 1
-            end
+            return math.tointeger(lFromRaw), math.tointeger(lToRaw), math.tointeger(rFromRaw), math.tointeger(rToRaw)
         end
     end
-    return countIncludedIntervals
+end
+
+function day4.countIncludedIntervals(filename)
+    local count = 0
+    for lFrom, lTo, rFrom, rTo in day4.rows(filename) do
+        local leftContainsRight = lFrom <= rFrom and lTo >= rTo
+        local rightContainsLeft = rFrom <= lFrom and rTo >= lTo
+        count = count + ((leftContainsRight or rightContainsLeft) and 1 or 0)
+    end
+    return count
 end
 
 function day4.countPartiallyIntersectedRanges(filename)
-    -- 2-4,6-8
-    regex = "(%d+)-(%d+),(%d+)-(%d+)"
-    local countIncludedIntervals = 0
-    for line in io.lines(filename) do
-        for lFromRaw, lToRaw, rFromRaw, rToRaw in string.gmatch(line, regex) do
-            lFrom, lTo, rFrom, rTo = math.tointeger(lFromRaw), math.tointeger(lToRaw), math.tointeger(rFromRaw), math.tointeger(rToRaw)
-            local leftContainsOneOfRight = lFrom <= rFrom and lTo >= rFrom or lFrom <= rTo and lTo >= rTo
-            local rightContainsOneOfLeft = rFrom <= lFrom and rTo >= lFrom or rFrom <= lTo and rTo >= lTo
-            if leftContainsOneOfRight or rightContainsOneOfLeft then
-                countIncludedIntervals = countIncludedIntervals + 1
-            end
-        end
+    local count = 0
+    for lFrom, lTo, rFrom, rTo in day4.rows(filename) do
+        local leftContainsOneOfRight = lFrom <= rFrom and lTo >= rFrom or lFrom <= rTo and lTo >= rTo
+        local rightContainsOneOfLeft = rFrom <= lFrom and rTo >= lFrom or rFrom <= lTo and rTo >= lTo
+        count = count + ((leftContainsOneOfRight or rightContainsOneOfLeft) and 1 or 0)
     end
-    return countIncludedIntervals
+    return count
 end
 
 
